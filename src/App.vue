10df25505ebcce34721d76cdf89971973774b27a
John Poom <drpoom@example.com>
2026-04-15 22:29:58 +0200

    v4.3.9: version bump

--- src/App.vue
+++ src/App.vue
@@ -141,7 +141,7 @@ import { useCurve } from './composables/useCurve.js'
 import { useMic } from './composables/useMic.js'
 
 // Version - Update this for each release
-const VERSION = 'v4.3.8';
+const VERSION = 'v4.3.9';
 
 // Score & High Score refs
 const score = ref(0);
